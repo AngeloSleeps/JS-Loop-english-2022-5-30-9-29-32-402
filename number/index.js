805d650ee09461349fd25562db30c9853bb80cae
Marcelo Magday <marcelo.angelo.magday@oocl.com>
2022-08-22 14:52:50 +0800

    Did JS-Loop task

---
+++ number/index.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 20; i++) {
+    if ((i + 1) % 2 == 0) {
+        console.log((i + 1) + ' is even number.');
+    } else {
+        console.log((i + 1) + ' is odd number.')
+    }
+}
